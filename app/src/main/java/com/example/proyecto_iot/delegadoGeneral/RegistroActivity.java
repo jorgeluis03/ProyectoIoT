d3d0b4a02734dda5c39efe682897e1743be2a4bc
noe <noe.jaraq@gmail.com>
2023-09-15 23:23:30 -0500

    cambio de nombre de LoginActivity -> IngresarActivity

--- app/src/main/java/com/example/proyecto_iot/delegadoGeneral/RegistroActivity.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoGeneral/RegistroActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.proyecto_iot.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registro);
     }
 }
