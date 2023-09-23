1e76562e2b88a281b87ebec49fda6b644bdfbaf5
Gustavo <a20192858@pucp.edu.pe>
2023-09-23 13:30:40 -0500

    cambio en dashboard_alumno

--- app/src/main/java/com/example/proyecto_iot/alumno/Alumno_Inicio_Todos.java
+++ app/src/main/java/com/example/proyecto_iot/alumno/Alumno_Inicio_Todos.java
@@ -1,8 +1,11 @@
 package com.example.proyecto_iot.alumno;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.proyecto_iot.R;
 
@@ -12,5 +15,14 @@ public class Alumno_Inicio_Todos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumno_inicio_todos);
+        // Obteniendo referencias a los TextView
+        TextView txtTodos = findViewById(R.id.txtTodos);
+        TextView txtPersonal = findViewById(R.id.txtPersonal);
+        // Cambiar el texto
+        txtTodos.setText("NuevoTextoTodos");
+        txtPersonal.setText("NuevoTextoPersonal");
+        // Aplicar el subrayado
+        txtTodos.setPaintFlags(txtTodos.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        txtPersonal.setPaintFlags(txtPersonal.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
