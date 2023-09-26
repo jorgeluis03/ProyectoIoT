0523eb77f6c097ae0bcc0987090d4228e3ce6f7e
jorgeluis <jorgel.dominguez03@gmail.com>
2023-09-26 12:45:38 -0500

    entitys para recycle view

--- app/src/main/java/com/example/proyecto_iot/delegadoGeneral/Dg_Activity.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoGeneral/Dg_Activity.java
@@ -1,11 +1,16 @@
 package com.example.proyecto_iot.delegadoGeneral;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.proyecto_iot.R;
 import com.example.proyecto_iot.databinding.ActivityDgBinding;
@@ -20,6 +25,10 @@ public class Dg_Activity extends AppCompatActivity {
         binding = ActivityDgBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //Mapear el toolbar como ActionBar
+        Toolbar toolbar = binding.toolbarActividadesDg;
+        setSupportActionBar(toolbar);
+        //================================
         buttomnavigationDg = binding.buttomnavigationDg;
         //Cargar el navigationComponent (navHost) en el bottomnavigation
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment_dg);
@@ -27,5 +36,29 @@ public class Dg_Activity extends AppCompatActivity {
         NavigationUI.setupWithNavController(buttomnavigationDg,navController);
         //===============================================================
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_dg,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.buscar_dg){
+            Toast.makeText(this,"Buscar actividad",Toast.LENGTH_SHORT).show();
+        }
+        if (id==R.id.perfil_dg){
+            Toast.makeText(this,"Perfil",Toast.LENGTH_SHORT).show();
+        }
+        if (id==R.id.configuracion_dg){
+            Toast.makeText(this,"Configuracion",Toast.LENGTH_SHORT).show();
+        }
+        if (id==R.id.cerrarSesion_dg){
+            Toast.makeText(this,"Salir",Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/proyecto_iot/delegadoGeneral/Dg_actividadesFragment.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoGeneral/Dg_actividadesFragment.java
@@ -3,64 +3,37 @@ package com.example.proyecto_iot.delegadoGeneral;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.databinding.FragmentDgActividadesBinding;
+import com.example.proyecto_iot.delegadoGeneral.adapter.ListaEmpleadosAdapter;
+import com.example.proyecto_iot.delegadoGeneral.entity.Empleado;
+import com.example.proyecto_iot.delegadoGeneral.entity.EmpleadoDto;
+import com.example.proyecto_iot.delegadoGeneral.retrofitServices.EmpleadoService;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Dg_actividadesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Dg_actividadesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Dg_actividadesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Dg_actividadesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Dg_actividadesFragment newInstance(String param1, String param2) {
-        Dg_actividadesFragment fragment = new Dg_actividadesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    FragmentDgActividadesBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dg_actividades, container, false);
+        binding=FragmentDgActividadesBinding.inflate(inflater,container,false);
+
+
+        return binding.getRoot();
     }
+
 }
