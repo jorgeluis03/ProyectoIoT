5c7757f4a7bc3f0060ba24c992def68db929ee93
jorgeluis <jorgel.dominguez03@gmail.com>
2023-09-26 18:48:45 -0500

    dg con recycle view

--- app/src/main/java/com/example/proyecto_iot/delegadoGeneral/Dg_actividadesFragment.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoGeneral/Dg_actividadesFragment.java
@@ -3,20 +3,75 @@ package com.example.proyecto_iot.delegadoGeneral;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.proyecto_iot.databinding.FragmentDgActividadesBinding;
+import com.example.proyecto_iot.delegadoGeneral.adapter.ListaEmpleadosAdapter;
+import com.example.proyecto_iot.delegadoGeneral.entity.Empleado;
+import com.example.proyecto_iot.delegadoGeneral.entity.EmpleadoDto;
+import com.example.proyecto_iot.delegadoGeneral.retrofitServices.EmpleadoService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Dg_actividadesFragment extends Fragment {
     FragmentDgActividadesBinding binding;
+    EmpleadoService empleadoService;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding=FragmentDgActividadesBinding.inflate(inflater,container,false);
 
-
+        obtenerData();
         return binding.getRoot();
     }
+
+    public void obtenerData(){
+        empleadoService = new Retrofit.Builder()
+                .baseUrl("http://10.100.251.61:8080")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(EmpleadoService.class);
+
+
+        empleadoService.obtenerLista().enqueue(new Callback<EmpleadoDto>() {
+            @Override
+            public void onResponse(Call<EmpleadoDto> call, Response<EmpleadoDto> response) {
+                if (response.isSuccessful()){
+                    EmpleadoDto empleadoDto = response.body();
+                    Log.d("response",empleadoDto.getEstado());
+                    List<Empleado> lista = empleadoDto.getLista();
+
+                    ListaEmpleadosAdapter adapter = new ListaEmpleadosAdapter();
+                    adapter.setContext(getContext());
+                    adapter.setListaEmpleados(lista);
+
+                    binding.recycleViewActividadesDg.setAdapter(adapter);
+                    binding.recycleViewActividadesDg.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+                }else {
+                    Log.d("response","Repuesta vacia");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<EmpleadoDto> call, Throwable t) {
+                Log.d("error","paso algo!");
+                Log.d("error",t.getMessage());
+
+            }
+        });
+    }
+
 
 }
--- app/src/main/java/com/example/proyecto_iot/delegadoGeneral/adapter/ListaEmpleadosAdapter.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoGeneral/adapter/ListaEmpleadosAdapter.java
@@ -1,7 +1,70 @@
 package com.example.proyecto_iot.delegadoGeneral.adapter;
 
-public class ListaEmpleadosAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.delegadoGeneral.entity.Empleado;
+
+import java.util.List;
+
+public class ListaEmpleadosAdapter extends RecyclerView.Adapter<ListaEmpleadosAdapter.EmpleadoViewHolder> {
+    private List<Empleado> listaEmpleados;
+    private Context context;
+
+    @NonNull
+    @Override
+    public EmpleadoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.irv_dg_actividades,parent,false);
+        return new EmpleadoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EmpleadoViewHolder holder, int position) {
+        Empleado e = listaEmpleados.get(position);
+        holder.empleado = e;
+
+        TextView textViewNombreActivity = holder.itemView.findViewById(R.id.textViewNombreActividad_dg);
+        TextView textViewCorreo = holder.itemView.findViewById(R.id.textViewCorreo_dg);
+
+        textViewNombreActivity.setText(e.getFirstName()+' '+e.getLastName());
+        textViewCorreo.setText(e.getPhoneNumber());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaEmpleados.size();
+    }
+
+    public class EmpleadoViewHolder extends RecyclerView.ViewHolder{
+        Empleado empleado;
+        public EmpleadoViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    //Encapsulamiento
+    public List<Empleado> getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(List<Empleado> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
 }
 
