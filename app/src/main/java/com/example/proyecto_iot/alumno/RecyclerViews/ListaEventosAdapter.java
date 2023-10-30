34e2fe384a8b3b0b2e0f093fc38f00d63a5214ea
noe <noe.jaraq@gmail.com>
2023-10-30 16:04:48 -0500

    cargadno foto de evento

--- app/src/main/java/com/example/proyecto_iot/alumno/RecyclerViews/ListaEventosAdapter.java
+++ app/src/main/java/com/example/proyecto_iot/alumno/RecyclerViews/ListaEventosAdapter.java
@@ -10,12 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.proyecto_iot.R;
 import com.example.proyecto_iot.alumno.AlumnoEventoActivity;
 import com.example.proyecto_iot.alumno.Entities.Evento;
@@ -44,12 +48,20 @@ public class ListaEventosAdapter extends RecyclerView.Adapter<ListaEventosAdapte
         TextView textDescripcion = holder.itemView.findViewById(R.id.textDescripcion);
         TextView textFecha = holder.itemView.findViewById(R.id.textFecha);
         TextView textHora = holder.itemView.findViewById(R.id.textHora);
+        ImageView imageEvento = holder.itemView.findViewById(R.id.imageEvento);
 
         textTitulo.setText(evento.getTitulo());
         textActividad.setText(evento.getActividad());
         textDescripcion.setText(evento.getDescripcion());
         textFecha.setText(evento.getFecha());
         textHora.setText(evento.getHora());
+
+        String fotoUrl = evento.getFotoUrl();
+        RequestOptions requestOptions = new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL); // Almacenamiento en cache
+        Glide.with(getContext())
+                .load(fotoUrl)
+                .apply(requestOptions)
+                .into(imageEvento);
     }
 
     @Override
