da2c3c55777f886be5c53bb26283dea991c9d32e
noe <noe.jaraq@gmail.com>
2023-10-30 19:33:06 -0500

    avance ejecucion de accion al dar enter

--- app/src/main/java/com/example/proyecto_iot/alumno/AlumnoBuscarEventoActivity.java
+++ app/src/main/java/com/example/proyecto_iot/alumno/AlumnoBuscarEventoActivity.java
@@ -3,14 +3,35 @@ package com.example.proyecto_iot.alumno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.databinding.ActivityAlumnoBuscarEventoBinding;
 
 public class AlumnoBuscarEventoActivity extends AppCompatActivity {
+    private ActivityAlumnoBuscarEventoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alumno_buscar_evento);
+        binding = ActivityAlumnoBuscarEventoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonBack.setOnClickListener(view -> {
+            finish();
+        });
+
+        binding.inputBuscarEvento.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (i == KeyEvent.KEYCODE_ENTER)){
+                    Toast.makeText(AlumnoBuscarEventoActivity.this, "ga", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
