804d556c8129df09f7bb0ab6a3978392595afc38
noe <noe.jaraq@gmail.com>
2023-11-04 03:06:27 -0500

    opciones de apoyar y desapoyar agregadas, falta recargar activity cuando estas acciones ocurren

--- app/src/main/java/com/example/proyecto_iot/alumno/Fragments/AlumnoApoyandoButtonFragment.java
+++ app/src/main/java/com/example/proyecto_iot/alumno/Fragments/AlumnoApoyandoButtonFragment.java
@@ -14,16 +14,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.cometchat.chat.core.AppSettings;
+import com.cometchat.chat.core.CometChat;
+import com.cometchat.chat.exceptions.CometChatException;
+import com.example.proyecto_iot.AppConstants;
 import com.example.proyecto_iot.R;
 import com.example.proyecto_iot.alumno.AlumnoChatActivity;
 import com.example.proyecto_iot.alumno.AlumnoEventoActivity;
 import com.example.proyecto_iot.alumno.Entities.Evento;
 import com.example.proyecto_iot.databinding.FragmentAlumnoApoyandoButtonBinding;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AlumnoApoyandoButtonFragment extends Fragment {
 
     FragmentAlumnoApoyandoButtonBinding binding;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String userUid = FirebaseAuth.getInstance().getUid();
+    private Evento evento;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,9 +42,10 @@ public class AlumnoApoyandoButtonFragment extends Fragment {
             mostrarConfirmacionDialog();
         });
 
+        evento = ((AlumnoEventoActivity) getActivity()).getEvento();
+
         binding.buttonAbrirChat.setOnClickListener(view -> {
             Intent intent = new Intent(getContext(), AlumnoChatActivity.class);
-            Evento evento = ((AlumnoEventoActivity) getActivity()).getEvento();
             intent.putExtra("evento", evento);
             startActivity(intent);
         });
@@ -53,7 +63,7 @@ public class AlumnoApoyandoButtonFragment extends Fragment {
                 desapoyarEvento();
             }
         });
-        alertDialog.setNeutralButton("No", new DialogInterface.OnClickListener() {
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 
@@ -63,14 +73,73 @@ public class AlumnoApoyandoButtonFragment extends Fragment {
     }
 
     private void desapoyarEvento(){
+        quitarAlumnoDeEvento();
+    }
 
-        // quitar alumno de lista de apoyos del evento
+    private void quitarAlumnoDeEvento(){
+        db.collection("eventos")
+                .document("evento"+evento.getFechaHoraCreacion().toString())
+                .collection("apoyos")
+                .document(userUid)
+                .delete()
+                .addOnSuccessListener(unused -> {
+                   Log.d("msg-test", "alumno quitado de eventos");
+                   quitarEventoDeAlumno();
+                })
+                .addOnFailureListener(e -> {
+                    e.printStackTrace();
+                });
+    }
 
-        // quitar evento de lista eventos de alumno
+    private void quitarEventoDeAlumno(){
+        db.collection("alumnos")
+                .document(userUid)
+                .collection("eventos")
+                .document("evento"+evento.getFechaHoraCreacion().toString())
+                .delete()
+                .addOnSuccessListener(unused -> {
+                    Log.d("msg-test", "evento quitado de alumno");
+                    quitarAlumnoDeChat();
+                })
+                .addOnFailureListener(e -> {
+                    e.printStackTrace();
+                });
+    }
 
-        // quitar alumno de grupo de cometchat
+    private void quitarAlumnoDeChat(){
+        String region = AppConstants.REGION;
+        String appID = AppConstants.APP_ID;
 
-        ((AlumnoEventoActivity)getActivity()).recreate();
-        Log.d("msg-test", "recargado");
+        AppSettings appSettings = new AppSettings.AppSettingsBuilder()
+                .subscribePresenceForAllUsers()
+                .setRegion(region)
+                .autoEstablishSocketConnection(true)
+                .build();
+
+        CometChat.init(getContext(), appID, appSettings, new CometChat.CallbackListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+
+                CometChat.leaveGroup(evento.getChatID(), new CometChat.CallbackListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        Log.d("msg-test", s);
+
+                        // TODO: RECARGAR ACTIVITY PARA ACTUALIZAR BOTONES
+                    }
+
+                    @Override
+                    public void onError(CometChatException e) {
+                        Log.d("msg-test", "Group leaving failed with exception: " + e.getMessage());
+                    }
+                });
+
+            }
+
+            @Override
+            public void onError(CometChatException e) {
+                Log.d("msg-test", "Initialization failed with exception: " + e.getMessage());
+            }
+        });
     }
 }
