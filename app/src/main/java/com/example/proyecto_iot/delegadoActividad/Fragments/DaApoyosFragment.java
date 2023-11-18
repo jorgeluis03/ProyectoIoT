39f00605cbb0472680822effe84d0eb686d0012c
amanr <amanrique068@gmail.com>
2023-11-18 10:11:38 -0500

    intentando listar apoyos

--- app/src/main/java/com/example/proyecto_iot/delegadoActividad/Fragments/DaApoyosFragment.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoActividad/Fragments/DaApoyosFragment.java
@@ -2,65 +2,110 @@ package com.example.proyecto_iot.delegadoActividad.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.alumno.AlumnoEventoActivity;
+import com.example.proyecto_iot.alumno.Entities.Alumno;
+import com.example.proyecto_iot.alumno.Entities.Evento;
+import com.example.proyecto_iot.databinding.FragmentDaApoyosBinding;
+import com.example.proyecto_iot.delegadoActividad.Entities.ApoyoDto;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DaApoyosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class DaApoyosFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DaApoyosFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DaApoyosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DaApoyosFragment newInstance(String param1, String param2) {
-        DaApoyosFragment fragment = new DaApoyosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentDaApoyosBinding binding;
+    private Evento evento;
+    private ApoyoDto apoyo;
+    private ArrayList<ApoyoDto> apoyos = new ArrayList<>();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        binding = FragmentDaApoyosBinding.inflate(inflater, container, false);
+        evento = ((AlumnoEventoActivity) getActivity()).getEvento();
+        Log.d("msg-test","DaApoyos: "+evento.getFechaHoraCreacion());
+        cargarFragment();
+
+        apoyos = new ArrayList<>();
+        apoyos = cargarApoyos(evento);
+
+        String apoyosStr;
+        if (apoyos.size()==0){
+            apoyosStr = "Sin apoyos";
+        } else if (apoyos.size()==1) {
+            apoyosStr = "1 apoyo";
+        }else {
+            apoyosStr = apoyos.size() + " apoyos";
+        }
+        binding.buttonApoyos.setText(apoyosStr);
+
         return inflater.inflate(R.layout.fragment_da_apoyos, container, false);
+    }
+
+    private void cargarFragment() {
+        if (evento.getEstado().equals("inactivo")){
+            binding.buttonApoyos.setVisibility(View.GONE);
+        }
+    }
+    private ArrayList<ApoyoDto> cargarApoyos(Evento evento) {
+        Log.d("msg-test", "evento"+evento.getFechaHoraCreacion().toString());
+        db.collection("eventos")
+                .document("evento"+evento.getFechaHoraCreacion().toString())
+                .collection("apoyos")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("msg-test", "busqueda apoyos ok: "+task.getResult().size());
+                            for (QueryDocumentSnapshot document: task.getResult()){
+                                apoyo = new ApoyoDto();
+                                apoyo = buscarAlumno(document.getId());
+                                apoyo.setCategoria(document.getString("categoria"));
+                                apoyo.setEventoId(evento.getFechaHoraCreacion().toString());
+                                apoyos.add(apoyo);
+                            }
+                        } else {
+                            Log.d("msg-test", "AlumnoEventoActivity: error al buscar evento");
+                        }
+                    }
+                });
+        return apoyos;
+    }
+    private ApoyoDto buscarAlumno(String alumnoId){
+        ApoyoDto apoyo = new ApoyoDto();
+        db.collection("alumnos")
+                .document(alumnoId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            Alumno alumno = task.getResult().toObject(Alumno.class);
+                            if (alumno.getEstado().equals("activo")){
+                                Log.d("msg-test", "apoyo encontrado: "+alumno.getNombre());
+                                apoyo.setAlumno(alumno);
+                            }
+                        }
+                        else{
+                            Log.d("msg-test", "ListaApoyos error buscando apoyo: "+alumnoId);
+                        }
+                    }
+                });
+        return apoyo;
     }
 }
