85395d30d103f5b698d0d1202aeb3cbfadee3945
noe <noe.jaraq@gmail.com>
2023-12-08 16:36:41 -0500

    boton de subir foto solo cuando evento activo

--- app/src/main/java/com/example/proyecto_iot/alumno/AlumnoEventoActivity.java
+++ app/src/main/java/com/example/proyecto_iot/alumno/AlumnoEventoActivity.java
@@ -86,7 +86,7 @@ public class AlumnoEventoActivity extends AppCompatActivity {
         binding.progressBar5.setVisibility(View.VISIBLE);
         evento = (Evento) getIntent().getSerializableExtra("evento");
         user = getIntent().getStringExtra("userUid");
-        Log.d("msg-test","obteniendo: "+evento.getDelegado()+" a "+user);
+        Log.d("msg-test", "obteniendo: " + evento.getDelegado() + " a " + user);
 
         cargarInfoEvento();
         cargarFotos();
@@ -194,30 +194,24 @@ public class AlumnoEventoActivity extends AppCompatActivity {
                         Log.d("msg-test", "Listen failed in evento activity");
                         return;
                     }
-                    if (savedInstanceState == null) { // activity recien creada
-                        if (!isFromDelegado){
-                            binding.fragmentApoyos.setVisibility(View.GONE);
-                            if (value != null && value.exists()) { // evento en lista de eventos de alumno (evento apoyado)
-                                getSupportFragmentManager().beginTransaction()
-                                        .setReorderingAllowed(true)
-                                        .replace(R.id.fragmentEventoButtons, AlumnoApoyandoButtonFragment.class, null)
-                                        .commit();
-                                binding.buttonSubirFotos.setVisibility(View.VISIBLE);
-                            } else { // evento no apoyado
-                                getSupportFragmentManager().beginTransaction()
-                                        .setReorderingAllowed(true)
-                                        .replace(R.id.fragmentEventoButtons, AlumnoApoyarButtonFragment.class, null)
-                                        .commit();
-                            }
-                        }else {
-                            binding.fragmentEventoButtons.setVisibility(View.GONE);
-                            binding.fragmentApoyos.setVisibility(View.VISIBLE);
-                            if (!evento.getEstado().equals("activo")){
-                                binding.buttonSubirFotos.setVisibility(View.GONE);
-                            }else {
-                                binding.buttonSubirFotos.setVisibility(View.VISIBLE);
-                                binding.buttonEditFloating.setVisibility(View.VISIBLE);
-                            }
+                    if (!isFromDelegado) {
+                        binding.fragmentApoyos.setVisibility(View.GONE);
+                        if (value != null && value.exists()) { // evento en lista de eventos de alumno (evento apoyado)
+                            getSupportFragmentManager().beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .replace(R.id.fragmentEventoButtons, AlumnoApoyandoButtonFragment.class, null)
+                                    .commit();
+                        } else { // evento no apoyado
+                            getSupportFragmentManager().beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .replace(R.id.fragmentEventoButtons, AlumnoApoyarButtonFragment.class, null)
+                                    .commit();
+                        }
+                    } else {
+                        binding.fragmentEventoButtons.setVisibility(View.GONE);
+                        binding.fragmentApoyos.setVisibility(View.VISIBLE);
+                        if (evento.getEstado().equals("activo")) {
+                            binding.buttonEditFloating.setVisibility(View.VISIBLE);
                         }
                     }
                 }));
@@ -251,9 +245,9 @@ public class AlumnoEventoActivity extends AppCompatActivity {
                         Tasks.whenAllComplete(tasks)
                                 .addOnCompleteListener(allTasks -> {
                                     binding.progressBar5.setVisibility(View.GONE);
-                                    if (fotoList.isEmpty()){
+                                    if (fotoList.isEmpty()) {
                                         binding.textView26.setVisibility(View.VISIBLE);
-                                    }else {
+                                    } else {
                                         adapter.notifyDataSetChanged();
                                     }
                                 });
@@ -345,7 +339,7 @@ public class AlumnoEventoActivity extends AppCompatActivity {
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         Alumno alumno = task.getResult().toObject(Alumno.class);
-                        binding.textDelegadoActividad.setText(alumno.getNombre()+" "+alumno.getApellidos());
+                        binding.textDelegadoActividad.setText(alumno.getNombre() + " " + alumno.getApellidos());
                         cargarDelegadoDialog(alumno);
                     } else {
                         Log.d("msg-test", "error buscando delegado de evento: " + task.getException().getMessage());
@@ -358,7 +352,7 @@ public class AlumnoEventoActivity extends AppCompatActivity {
         binding.buttonEventoFecha.setText(evento.getFecha());
         binding.buttonEventoHora.setText(evento.getHora());
         binding.buttonEventoLugar.setText(evento.getLugar());
-        binding.textEstado2.setVisibility(evento.getEstado().equals("inactivo")?View.VISIBLE:View.GONE);
+        binding.textEstado2.setVisibility(evento.getEstado().equals("inactivo") ? View.VISIBLE : View.GONE);
 
 
         RequestOptions requestOptions = new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL); // Almacenamiento en cache
@@ -366,6 +360,10 @@ public class AlumnoEventoActivity extends AppCompatActivity {
                 .load(evento.getFotoUrl())
                 .apply(requestOptions)
                 .into(binding.imageEvento);
+
+        if (evento.getEstado().equals("inactivo")) {
+            binding.buttonSubirFotos.setVisibility(View.GONE);
+        }
     }
 
     private void crearEventoEnCalendario() {
@@ -397,11 +395,11 @@ public class AlumnoEventoActivity extends AppCompatActivity {
         }
     }
 
-    private void cargarDelegadoDialog(Alumno delegado){
+    private void cargarDelegadoDialog(Alumno delegado) {
         float density = getResources().getDisplayMetrics().density;
         Dialog dialog = new Dialog(AlumnoEventoActivity.this);
         dialog.setContentView(R.layout.dialog_delegado_actividad);
-        dialog.getWindow().setLayout((int) (320*density), ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setLayout((int) (320 * density), ViewGroup.LayoutParams.WRAP_CONTENT);
         dialog.getWindow().setBackgroundDrawable(AlumnoEventoActivity.this.getDrawable(R.drawable.shape_user_dialog));
 
         TextView textNombreDelegadoDialog = dialog.findViewById(R.id.textNombreDelegadoActividad);
@@ -409,7 +407,7 @@ public class AlumnoEventoActivity extends AppCompatActivity {
         TextView textCorreoDelegadoDialog = dialog.findViewById(R.id.textCorreoDelegadoActividad);
         ImageView imageDelegadoDialog = dialog.findViewById(R.id.imageDelegadoActividad);
 
-        textNombreDelegadoDialog.setText(delegado.getNombre()+" "+delegado.getApellidos());
+        textNombreDelegadoDialog.setText(delegado.getNombre() + " " + delegado.getApellidos());
         textCodigoDelegadoDialog.setText(delegado.getCodigo());
         textCorreoDelegadoDialog.setText(delegado.getCorreo());
         RequestOptions requestOptions = new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL); // Almacenamiento en cache
