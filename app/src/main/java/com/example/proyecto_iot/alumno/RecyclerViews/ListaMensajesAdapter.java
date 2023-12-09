b8d519a9c7fbf88f52e28283c2f11f48eaf7fb53
noe <noe.jaraq@gmail.com>
2023-12-09 18:43:11 -0500

    hora y nombre de user en msjs

--- app/src/main/java/com/example/proyecto_iot/alumno/RecyclerViews/ListaMensajesAdapter.java
+++ app/src/main/java/com/example/proyecto_iot/alumno/RecyclerViews/ListaMensajesAdapter.java
@@ -11,10 +11,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.alumno.Entities.Alumno;
 import com.example.proyecto_iot.alumno.Entities.ChatMessage;
 import com.example.proyecto_iot.delegadoGeneral.utils.FirebaseUtilDg;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+import java.text.SimpleDateFormat;
 
 public class ListaMensajesAdapter extends FirestoreRecyclerAdapter<ChatMessage, ListaMensajesAdapter.ViewHolder> {
     private Context context;
@@ -25,15 +28,34 @@ public class ListaMensajesAdapter extends FirestoreRecyclerAdapter<ChatMessage, 
 
     @Override
     protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull ChatMessage model) {
+
+        // formateando hora
+        String pattern = "HH:mm";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String horaMensaje = simpleDateFormat.format(model.getTimestamp().toDate());
+
         if (model.getSenderID().equals(FirebaseUtilDg.getusuarioActualId())){
             holder.leftChatLayout.setVisibility(View.GONE);
             holder.rightChatLayout.setVisibility(View.VISIBLE);
+
             holder.rightChatText.setText(model.getChatMessage());
+            holder.rightChatTime.setText(horaMensaje);
         }
         else{
             holder.rightChatLayout.setVisibility(View.GONE);
             holder.leftChatLayout.setVisibility(View.VISIBLE);
+
             holder.leftChatText.setText(model.getChatMessage());
+            holder.leftChatTime.setText(horaMensaje);
+
+            FirebaseUtilDg.getCollAlumnos().document(model.getSenderID()).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()){
+                    Alumno alumno = task.getResult().toObject(Alumno.class);
+                    String nombreMensaje = alumno.getNombre()+" "+alumno.getApellidos().charAt(0);
+                    holder.leftChatUser.setText(nombreMensaje);
+                }
+            });
+
         }
     }
 
@@ -46,15 +68,19 @@ public class ListaMensajesAdapter extends FirestoreRecyclerAdapter<ChatMessage, 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         LinearLayout leftChatLayout, rightChatLayout;
-        TextView leftChatText, rightChatText;
+        TextView leftChatText, leftChatUser, leftChatTime, rightChatText, rightChatTime;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             leftChatLayout = itemView.findViewById(R.id.leftChatLayout);
             leftChatText = itemView.findViewById(R.id.leftChatText);
+            leftChatUser = itemView.findViewById(R.id.leftChatUser);
+            leftChatTime = itemView.findViewById(R.id.leftChatTime);
+
             rightChatLayout = itemView.findViewById(R.id.rightChatLayout);
             rightChatText = itemView.findViewById(R.id.rightChatText);
+            rightChatTime = itemView.findViewById(R.id.rightChatTime);
         }
     }
 }
