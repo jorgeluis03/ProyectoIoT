de393539aec62ce8c14f433c6ee9e099a70d0f8f
noe <noe.jaraq@gmail.com>
2023-12-10 18:01:57 -0500

    progress bar editar perfil

--- app/src/main/java/com/example/proyecto_iot/alumno/AlumnoPerfilEditarActivity.java
+++ app/src/main/java/com/example/proyecto_iot/alumno/AlumnoPerfilEditarActivity.java
@@ -11,6 +11,7 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -89,7 +90,6 @@ public class AlumnoPerfilEditarActivity extends AppCompatActivity {
 
             }
         });
-
         binding.inputApellidos.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -125,7 +125,6 @@ public class AlumnoPerfilEditarActivity extends AppCompatActivity {
         });
 
         binding.buttonGuardarPerfil.setOnClickListener(view -> {
-            binding.buttonGuardarPerfil.setEnabled(false);
             guardarPerfil();
         });
     }
@@ -149,6 +148,8 @@ public class AlumnoPerfilEditarActivity extends AppCompatActivity {
     }
 
     private void guardarPerfil() {
+        binding.buttonGuardarPerfil.setEnabled(false);
+        binding.progressBarGuardarPerfil.setVisibility(View.VISIBLE);
 
         if (nuevaFoto){
             actualizarFotoEInfo();
@@ -218,6 +219,7 @@ public class AlumnoPerfilEditarActivity extends AppCompatActivity {
                         actualizarInternalStorage();
 
                         // regresar a perfil activity
+                        binding.progressBarGuardarPerfil.setVisibility(View.GONE);
                         finish();
                     }
                 })
@@ -249,10 +251,8 @@ public class AlumnoPerfilEditarActivity extends AppCompatActivity {
 
     private void cargarFoto() {
         String url = alumno.getFotoUrl().equals("")? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png": alumno.getFotoUrl();
-        RequestOptions requestOptions = new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL); // Almacenamiento en cache
         Glide.with(AlumnoPerfilEditarActivity.this)
                 .load(url)
-                .apply(requestOptions)
                 .into(binding.imageEdit);
     }
 
