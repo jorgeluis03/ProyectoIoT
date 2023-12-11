7e8117f6a60fa7a072dcae09ffd677af310a37dd
jorgeluis <jorgel.dominguez03@gmail.com>
2023-12-11 15:55:35 -0500

    bottomshet

--- app/src/main/java/com/example/proyecto_iot/delegadoGeneral/adapter/ListaUsuariosRegisAdpter.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoGeneral/adapter/ListaUsuariosRegisAdpter.java
@@ -1,27 +1,42 @@
 package com.example.proyecto_iot.delegadoGeneral.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.proyecto_iot.R;
 import com.example.proyecto_iot.alumno.Entities.Alumno;
 import com.example.proyecto_iot.delegadoGeneral.utils.AndroidUtilDg;
 import com.example.proyecto_iot.delegadoGeneral.utils.FirebaseUtilDg;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 
 public class ListaUsuariosRegisAdpter extends FirestoreRecyclerAdapter<Alumno,ListaUsuariosRegisAdpter.DelegadoViewHolder> {
     Context context;
+
+
+    String motivo;
 
     public ListaUsuariosRegisAdpter(@NonNull FirestoreRecyclerOptions<Alumno> options,Context context) {
         super(options);
@@ -33,25 +48,8 @@ public class ListaUsuariosRegisAdpter extends FirestoreRecyclerAdapter<Alumno,Li
         holder.tvCorreoUser.setText(model.getCorreo());
 
         holder.buttonBanear.setOnClickListener(view -> {
-            new MaterialAlertDialogBuilder(context)
-                    .setTitle("Banear")
-                    .setMessage("¿Estás seguro que deseas banear a este usuario?")
-                    .setNeutralButton("Cancelar", (dialog, which) -> {
+            showDialog(model);
 
-                    })
-                    .setPositiveButton("Aceptar", (dialog, which) -> {
-                        // Responder a la pulsación del botón positivo
-                        FirebaseUtilDg.getCollAlumnos().document(model.getId())
-                        .update("estado","baneado")
-                                .addOnSuccessListener(unused -> {
-                                    Toast.makeText(context,"Usuario baneado",Toast.LENGTH_SHORT).show();
-                                })
-                                .addOnFailureListener(e ->
-                                        Toast.makeText(context,"Error",Toast.LENGTH_SHORT).show());
-
-                        AndroidUtilDg.enviarCorreo(model,"baneado");
-                    })
-                    .show();
         });
     }
 
@@ -76,4 +74,55 @@ public class ListaUsuariosRegisAdpter extends FirestoreRecyclerAdapter<Alumno,Li
 
         }
     }
+
+    public void showDialog(Alumno model) {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
+        View dialogView = LayoutInflater.from(context).inflate(R.layout.dg_dialog_layout, null);
+        bottomSheetDialog.setContentView(dialogView);
+
+        // Obtener referencias a los elementos del layout
+        TextView textViewMiembros = dialogView.findViewById(R.id.textViewMiembros);
+        RadioGroup radioGroup = dialogView.findViewById(R.id.radioGroup);
+        RadioButton radioButtonOption1 = dialogView.findViewById(R.id.radioButtonOption1);
+        RadioButton radioButtonOption2 = dialogView.findViewById(R.id.radioButtonOption2);
+        // Agregar un OnCheckedChangeListener al RadioGroup
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+
+            if(checkedId==R.id.radioButtonOption1){
+                motivo= radioButtonOption1.getText().toString();
+                bottomSheetDialog.dismiss();
+
+            }else if (checkedId == R.id.radioButtonOption2) {
+                motivo= radioButtonOption2.getText().toString();
+
+            }
+            bottomSheetDialog.dismiss();
+            new MaterialAlertDialogBuilder(context)
+                    .setTitle("Banear")
+                    .setMessage("¿Estás seguro que deseas banear a este usuario por "+motivo+"?")
+                    .setNeutralButton("Cancelar", (dialog, which) -> {
+
+                    })
+                    .setPositiveButton("Aceptar", (dialog, which) -> {
+                        // Responder a la pulsación del botón positivo
+                        FirebaseUtilDg.getCollAlumnos().document(model.getId())
+                                .update("estado","baneado")
+                                .addOnSuccessListener(unused -> {
+                                    Toast.makeText(context,"Usuario baneado",Toast.LENGTH_SHORT).show();
+                                })
+                                .addOnFailureListener(e ->
+                                        Toast.makeText(context,"Error",Toast.LENGTH_SHORT).show());
+
+                        AndroidUtilDg.enviarCorreo(model,"baneado");
+                    })
+                    .show();
+        });
+
+
+        // Mostrar el dialog
+        bottomSheetDialog.show();
+    }
+
+
+
 }
