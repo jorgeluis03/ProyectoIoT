0b4f2c8f06e44401d5ee32db51994caf89058911
amanr <amanrique068@gmail.com>
2023-12-11 09:06:56 -0500

    cierre de sesión forzado para delegadoactividad

--- app/src/main/java/com/example/proyecto_iot/delegadoActividad/DaInicioActivity.java
+++ app/src/main/java/com/example/proyecto_iot/delegadoActividad/DaInicioActivity.java
@@ -5,19 +5,28 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.alumno.AlumnoInicioActivity;
+import com.example.proyecto_iot.alumno.Entities.Alumno;
 import com.example.proyecto_iot.databinding.ActivityDaInicioBinding;
+import com.example.proyecto_iot.delegadoGeneral.entity.Actividades;
 import com.example.proyecto_iot.delegadoGeneral.utils.FirebaseUtilDg;
+import com.example.proyecto_iot.inicioApp.IngresarActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class DaInicioActivity extends AppCompatActivity {
 
     ActivityDaInicioBinding binding;
+    Alumno alumnoAutenticado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,49 @@ public class DaInicioActivity extends AppCompatActivity {
             if(task.isSuccessful()){
                 String token = task.getResult();
                 FirebaseUtilDg.getUsuarioActualDetalles().update("fcmToken", token);
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String uid = FirebaseAuth.getInstance().getUid();
+        FirebaseUtilDg.getCollAlumnos().document(uid).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                alumnoAutenticado = task.getResult().toObject(Alumno.class);
+                if (alumnoAutenticado.getEstado().equals("baneado")){
+                    cerrarSesion("Su cuenta ha sido baneada");
+                }
+                else if (alumnoAutenticado.getActividadesId()==null) {
+                    cerrarSesion("Ha dejado de ser delegado de actividad.");
+                } else if (alumnoAutenticado.getActividadesId()!=null) {
+                    boolean valido = false;
+                    for (Actividades a : alumnoAutenticado.getActividadesId()) {
+                        if (a.getEstado().equals("abierto")) {
+                            valido = true;
+                        }
+                    }
+                    if (!valido) {
+                        cerrarSesion("Ha dejado de ser delegado de actividad.");
+                    }
+                }
+            }
+            else{
+                Log.d("msg-test", "error al recuperar alumno AlumnoInicioActivity");
+            }
+        });
+    }
+    private void cerrarSesion(String mensaje){
+        FirebaseMessaging.getInstance().deleteToken().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+
+                FirebaseAuth.getInstance().signOut(); // deslogueo de firebase auth
+                Intent intent = new Intent(DaInicioActivity.this, IngresarActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
     }
